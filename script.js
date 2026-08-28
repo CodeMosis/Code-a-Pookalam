7c3787c16651be638350e5cfbef61dfe3797d541
Athira Binu <athira.binu013@gmail.com>
2026-08-28 18:44:23 +0530

    Added second flower with rotation and loops

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const petals0Container = document.querySelector(".petals0");
+const petals1Container = document.querySelector(".petals1");
 
 const numberOfPetals = 12;
 
@@ -7,7 +8,15 @@ const angle = 360 / numberOfPetals;
 for (let i = 0; i < numberOfPetals; i++) {
 
     const petal0 = document.createElement("div");
+    const petal1 = document.createElement("div");
+
     petal0.classList.add("petal0");
+    petal1.classList.add("petal1");
+
     petal0.style.transform = `rotate(${i * angle}deg)`;
+    petal1.style.transform = `rotate(${i * angle}deg)`;
+
     petals0Container.appendChild(petal0);
+    petals1Container.appendChild(petal1);
 }
+
